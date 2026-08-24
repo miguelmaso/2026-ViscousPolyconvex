57c0e004b7c9a024eb9a57ad2855e1cd2a9a94bd
miguelmaso <miguel.maso@upc.edu>
2026-08-24 09:20:34 +0200

    Updated calibration.jl

--- calibration/Calibration.jl
+++ calibration/Calibration.jl
@@ -3,14 +3,27 @@ using Optim
 using CSV
 using Plots
 
-experiments = CSV.read(joinpath(@__DIR__, "data", "quasi-static.csv"), UniaxialQuasiStaticTest)
+
+## Load data
+
+quasi_static_set = CSV.read(joinpath(@__DIR__, "data", "quasi-static.csv"), UniaxialQuasiStaticTest)
+cyclic_loading_set = CSV.read(joinpath(@__DIR__, "data", "cyclic_loading.csv"), UniaxialCyclicLoadingTest)
+
+
+## Equilibrium component
 
 build_model(μ) = NeoHookean3D(μ=μ, λ=0.0)
 pn = [  "μ"]  # Parameter names
 p0 = [  1e4]  # Initial seed
 
-f(p) = loss(build_model, p, experiments)
+f(p) = loss(build_model, p, quasi_static_set)
 
-result = optimize(f, p0, NelderMead())
-model = build_model(result.minimizer...)
-plot(model, experiments[1], xlabel="Stretch [-]", ylabel="Stress [KPa]", units_scale=1e-3)
+eq_result = optimize(f, p0, NelderMead())
+equilibrium_term = build_model(eq_result.minimizer...)
+plot(equilibrium_term, quasi_static_set[1], xlabel="Stretch [-]", ylabel="Stress [KPa]", units_scale=1e-3)
+
+
+## Non-equilibrium branches
+
+build_branch(μ, τ) = ViscousPolyconvex(μ=μ, τ=τ)
+
