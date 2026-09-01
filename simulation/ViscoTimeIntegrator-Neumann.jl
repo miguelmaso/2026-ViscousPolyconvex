71ce08480bde47e7252b20f72e96d3cfb1a83023
miguelmaso <miguel.maso@upc.edu>
2026-09-01 10:21:15 +0200

    Missing units in plot

--- simulation/ViscoTimeIntegrator-Neumann.jl
+++ simulation/ViscoTimeIntegrator-Neumann.jl
@@ -231,6 +231,7 @@ Wdiff = metrics.Dvis + metrics.KE + metrics.EE - metrics.Wext
 @show sum(metrics.Dvis) * problem_data.Δt
 
 areaplot(metrics.time, [metrics.Dvis metrics.KE metrics.EE], label=["Dissipation" "Kinetic" "Elastic"], alpha=0.8)
-plot!(metrics.time, metrics.Wext, label="External", color=:black, lw=3, style=:dot, legend=:outertop, legend_column=-1)
+plot!(metrics.time, metrics.Wext, label="External", color=:black, lw=3, style=:dot)
+plot!(xlabel="Time [s]", ylabel="Energy [J]", legend=:outertop, legend_column=-1)
 
 plot(metrics.time, Wdiff, lw=2, label="Dvis + Ψkin + Ψel - Wext")
